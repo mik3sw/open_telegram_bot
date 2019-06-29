0d0c9589c9d0ed860399973362eef42eaebed15b
Mike <42175526+MikeM2000@users.noreply.github.com>
2019-06-29 13:37:13 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,93 +1,121 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-#DEFINIZIONE COMANDI ADMIN
+# DEFINIZIONE COMANDI ADMIN
 from functools import wraps
+import wikipedia
 
 LIST_OF_ADMINS = [38201859]
 
-
-def restricted(func):
-    @wraps(func)
-    def wrapped(bot, update):
-        user_id = update.effective_user.id
-        if user_id not in LIST_OF_ADMINS:
-            print("Unauthorized access denied for [{}][{}].".format(user_id, update.message.from_user.username))
-            return
-        return func(bot, update)
-    return wrapped
-
 # FUNZIONI
 def cancellacomandi(bot, update):
-	if update.message.text is not None:
-		if update.message.text.startswith("/"):
-			bot.deleteMessage(chat_id=update.message.chat.id, message_id=update.message.message_id)
+    if update.message.text is not None:
+        if update.message.text.startswith("/"):
+            bot.deleteMessage(chat_id=update.message.chat.id, message_id=update.message.message_id)
 
 
 def admin(bot, update):
-	if update.message.text is not None:
-		if update.message.text.startswith("@admin"):
+    if update.message.text is not None:
+        if update.message.text.startswith("@admin"):
+            var_messaggio = update.message.text
+            var_messaggio = update.message.text[7:]
+            bot.send_message('@AOSPItaliaLOG',
+                             text="<b>NUOVA RICHIESTA DI SUPPORTO</b>\n\n<b>Gruppo</b>: {}\n<b>Autore</b>: @{username}\n<b>Messaggio</b>: <code>{}</code>\n\n<b>Vai al messaggio</b>: https://t.me/{}/{}"
+                             .format(update.message.chat.title, var_messaggio, update.message.chat.username, update.message.message_id, username=update.message.from_user.username, ),
+                             parse_mode='HTML')
+            bot.send_message(update.message.chat_id, "<code>Segnalazione effettuata!</code>", reply_to_message_id=update.message.message_id, parse_mode='HTML')
 
-			var_messaggio = update.message.text
-			var_messaggio = update.message.text[7:]
-			bot.send_message(-1001323415459,
-                 text="<b>NUOVA RICHIESTA DI SUPPORTO</b>\nAutore: {username}\n<code>Messaggio:{}</code>"
-					 .format(var_messaggio, username=update.message.from_user.username,),
-                 parse_mode='HTML')
-
-#RISPOSTE DI Gassistant
+# RISPOSTE DI Gassistant
 def okgoogle(bot, update):
-	if update.message.text is not None:
-		if str(update.message.text).lower() == "ok google":
-			bot.send_message(update.message.chat_id, "Ciao {name}, come posso aiutarti?".format(name=update.message.from_user.first_name))
+    if update.message.text is not None:
+        if str(update.message.text).lower() == "ok google":
+            bot.send_message(update.message.chat_id,
+                             "Ciao {name}, come posso aiutarti?".format(name=update.message.from_user.first_name))
+
 
 def nexus5x(bot, update):
-	if update.message.text is not None:
-		if 'nexus 5x' in str(update.message.text).lower():
-			bot.send_message.reply_text('Do you want some bootloop?')
-#Buongiorno
+    if update.message.text is not None:
+        if 'nexus 5x' in str(update.message.text).lower():
+            bot.send_message(update.message.chat_id, 'Do you want some bootloop?', reply_to_message_id=update.message.message_id)
+
+
+# Buongiorno
 def buongiorno(bot, update):
-	if update.message.text is not None:
-		if str(update.message.text).lower().startswith("buongiorno"):
-			bot.send_message(update.message.chat_id, "Buongiorno {username}".format(username=update.message.from_user.username))
-#Buonanotte
+    if update.message.text is not None:
+        if 'buongiorno' in str(update.message.text).lower():
+            bot.send_message(update.message.chat_id,
+                             "Buongiorno {username}".format(username=update.message.from_user.username), reply_to_message_id=update.message.message_id)
+
+
+# Buonanotte
 def buonanotte(bot, update):
-	if update.message.text is not None:
-		if str(update.message.text).lower().startswith("buonanotte"):
-			bot.send_message(update.message.chat_id, "Buonanotte {username}".format(username=update.message.from_user.username))
-			
+    if update.message.text is not None:
+        if 'buonanotte' in str(update.message.text).lower():
+            bot.send_message(update.message.chat_id,
+                             "Buonanotte {username}".format(username=update.message.from_user.username, reply_to_message_id=update.message.message_id))
 
-#BENVENUTO
+
+# BENVENUTO
 def welcome(bot, update):
-	for new in update.message.new_chat_members:
-		update.message.reply_text("Benvenuto {username} in {chat_title}!\n"
-								  "Ti preghiamo di rispettare le /regole per non ricevere sanzioni\n"
-								  "Buona permanenza!\n\n<em>- lo staff</em>".format(
-			username = "@"+new.username,
-			chat_title = update.message.chat.title
-			), parse_mode='HTML')
+    for new in update.message.new_chat_members:
+        update.message.reply_text("Benvenuto {username} in {chat_title}!\n"
+                                  "Ti preghiamo di rispettare le /regole per non ricevere sanzioni\n"
+                                  "Buona permanenza!\n\n<em>- lo staff</em>".format(
+            username="@" + new.username,
+            chat_title=update.message.chat.title
+        ), parse_mode='HTML')
+        bot.send_message('@AOSPItaliaLOG',
+                         text="<b>NUOVA UTENTE</b>\n\n<b>Gruppo</b>: {}\n<b>Utente</b>: @{username}\n\n<b>Vai al messaggio</b>: https://t.me/{}/{}"
+                         .format(update.message.chat.title, update.message.chat.username,
+                                 update.message.message_id, username=update.message.from_user.username, ),
+                         parse_mode='HTML')
 
 
+def say(bot, update):
+    if update.message.text is not None:
+        if update.message.text.startswith("/say"):
+            user_id = update.effective_user.id
+            if user_id in LIST_OF_ADMINS:
+                var_messaggio = update.message.text
+                var_messaggio = var_messaggio.replace("/say", "")
+                bot.send_message(update.message.chat_id, text='{}'.format(var_messaggio), parse_mode='HTML')
+
+def definisci(bot, update):
+    if update.message.text is not None:
+        if str(update.message.text).lower().startswith('definisci'):
+            var_messaggio = str(update.message.text).lower()
+            var_messaggio = var_messaggio.replace("definisci ", "")
+            print("{} searced a definition from Wikipedia".format(update.message.from_user.username))
+            wikipedia.set_lang("it")
+            try:
+                definition = wikipedia.summary(var_messaggio, sentences=3)
+                bot.sendMessage(update.message.chat_id, text=definition)
+            except:
+                bot.sendMessage(update.message.chat_id,
+                                text="Mi spiace {}, non ho trovato nulla riguardo '{}'".format(name, var_messaggio))
+
+
+'''''''''
 @restricted
 def say(bot, update):
 	pass
 	if update.message.text is not None:
 		if update.message.text.startswith("/say"):
-			var_messaggio = update.message.text
-			var_messaggio = var_messaggio.replace("/say", "")
-			bot.send_message(update.message.chat_id, text='{}'.format(var_messaggio), parse_mode='HTML')
+            user_id = update.effective_user.id
+            if user_id is in LIST_OF_ADMINS:
+			    var_messaggio = update.message.text
+			    var_messaggio = var_messaggio.replace("/say", "")
+			    bot.send_message(update.message.chat_id, text='{}'.format(var_messaggio), parse_mode='HTML')
+			    '''''''''
 
 
-#DICHIARAZIONE FUNZIONI
+# DICHIARAZIONE FUNZIONI
 def init(bot, update):
-	admin(bot, update)
-	welcome(bot, update)
-	okgoogle(bot, update)
-	buongiorno(bot, update)
-	buonanotte(bot, update)
-	say(bot, update)
-	#cancellacomandi(bot, update)
-
-
-
-	
-	
+    admin(bot, update)
+    welcome(bot, update)
+    okgoogle(bot, update)
+    buongiorno(bot, update)
+    buonanotte(bot, update)
+    say(bot, update)
+    nexus5x(bot, update)
+    definisci(bot, update)
+# cancellacomandi(bot, update)
